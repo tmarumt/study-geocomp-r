fa3797638cd8eada7870b2797bddaaf8f2d27ef3
tmarumt <tmarumt1@gmail.com>
2024-05-03 09:24:03 +0900

    feat: add subdirectory and volume for dvc-gdrive

--- .devcontainer/initialize.sh
+++ .devcontainer/initialize.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-volumes=("renv" "pip" "julia" "TinyTeX" "fonts")
+volumes=("renv" "pip" "julia" "TinyTeX" "fonts" "pydrive2fs")
 
 for vol in ${volumes[@]}; do
   if [ ! "$(docker volume ls -q -f name="$vol")" ]; then
